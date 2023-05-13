7d7ae3cad5b260b425fab083657d8e11d9706985
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-05-13 17:55:03 -0300

    Commit

---
+++ aula 04/first-project/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Este é meu primeiro projeto usando React e Vite</p>
+      <p>Proximos passos:</p>
+      <ul>
+        <li>Praticar com exercicios</li>
+        <li>Aprender a usar os dados dinamicos</li>
+        <li>Criar novo componentes</li>
+        <li>Estilizar os componetes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
+    </div>
+  );
+}
